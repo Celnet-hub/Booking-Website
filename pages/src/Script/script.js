638fa1951b609f6b0237599c6a9ac598c9fecf07
celestine <nwabuisicelestine@gmail.com>
2021-02-27 06:10:03 +0100

    Created HamburgerBtn functionality

--- pages/src/Script/script.js
+++ pages/src/Script/script.js
@@ -1,1 +1,14 @@
 console.log('Sass compiled');
+//Hamburger Button for Mobile view
+const HamburgerBtn = document.querySelector('#HamburgerBtn'); 
+
+HamburgerBtn.addEventListener('click', function () {
+    
+    if (HamburgerBtn.classList.contains('open')) {
+        HamburgerBtn.classList.remove('open');
+        console.log('Hamburger button has been clicked');
+    } else {
+        HamburgerBtn.classList.add('open');
+        console.log('Hamburger button has been clicked');
+    }
+});
