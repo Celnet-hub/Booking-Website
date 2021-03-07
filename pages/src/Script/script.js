97b78dc557fa9b3ed156dcbf9b4619d99450b32a
celestine <nwabuisicelestine@gmail.com>
2021-03-07 21:45:35 +0100

    modified the toggle menu

--- pages/src/Script/script.js
+++ pages/src/Script/script.js
@@ -4,11 +4,13 @@ const HamburgerBtn = document.querySelector("#HamburgerBtn");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector('body');
 
 HamburgerBtn.addEventListener("click", function () {
 	if (header.classList.contains("open")) {
 		//close hamburger menu
 		header.classList.remove("open");
+        body.classList.remove('noscroll')
 		fadeElems.forEach((element) => {
 			element.classList.remove("fade-in");
 			element.classList.add("fade-out");
@@ -16,6 +18,7 @@ HamburgerBtn.addEventListener("click", function () {
 	} else {
 		//open hamburger menu
 		header.classList.add("open");
+        body.classList.add('noscroll');
 		fadeElems.forEach((element) => {
 			element.classList.remove("fade-out");
 			element.classList.add("fade-in");
