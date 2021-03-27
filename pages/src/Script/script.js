3449cd50ebdbbb9150e251b0056cccbe9d2eaf15
celestine <nwabuisicelestine@gmail.com>
2021-03-27 07:09:22 +0100

    created Hero section

--- pages/src/Script/script.js
+++ pages/src/Script/script.js
@@ -4,24 +4,31 @@ const HamburgerBtn = document.querySelector("#HamburgerBtn");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
-const body = document.querySelector('body');
+const body = document.querySelector("body");
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
 
-HamburgerBtn.addEventListener("click", function () {
-	if (header.classList.contains("open")) {
-		//close hamburger menu
-		header.classList.remove("open");
-        body.classList.remove('noscroll')
-		fadeElems.forEach((element) => {
-			element.classList.remove("fade-in");
-			element.classList.add("fade-out");
-		});
-	} else {
-		//open hamburger menu
-		header.classList.add("open");
-        body.classList.add('noscroll');
-		fadeElems.forEach((element) => {
-			element.classList.remove("fade-out");
-			element.classList.add("fade-in");
-		});
-	}
+// HamburgerBtn.addEventListener("click", function () {
+// 	if (header.classList.contains("open")) {
+// 		//close hamburger menu
+// 		header.classList.remove("open");
+// 		body.classList.remove("noscroll");
+// 		fadeElems.forEach((element) => {
+// 			element.classList.remove("fade-in");
+// 			element.classList.add("fade-out");
+// 		});
+// 	} else {
+// 		//open hamburger menu
+// 		header.classList.add("open");
+// 		body.classList.add("noscroll");
+// 		fadeElems.forEach((element) => {
+// 			element.classList.remove("fade-out");
+// 			element.classList.add("fade-in");
+// 		});
+// 	}
+// });
+
+menuToggle.addEventListener("click", () => {
+	menuToggle.classList.toggle("active");
+	showcase.classList.toggle("active");
 });
